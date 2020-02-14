41e11c5f4c3a46a5e07fc53263f8504b11f6a552
shota <sy84.ztd.9@gmail.com>
2020-02-14 12:41:35 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_031144) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "stastus"
+    t.string "status"
   end
 
 end
