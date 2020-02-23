5973aa8c722388a0bc9edcc51c064ed34d2b24dc
shota <sy84.ztd.9@gmail.com>
2020-02-23 00:45:04 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
     def index
      @users = User.order(id: :desc).page(params[:page]).per(10)
     end
+    
+    include SessionsHelper
+    
+    private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
